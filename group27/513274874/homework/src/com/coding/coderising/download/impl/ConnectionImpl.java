c7f1071785c2c070ae8dda331422435882e20cf5
qqdcy <xueyy_123@163.com>
2017-04-03 10:46:55 +0800

    。。

--- group27/513274874/homework/src/com/coding/coderising/download/impl/ConnectionImpl.java
+++ group27/513274874/homework/src/com/coding/coderising/download/impl/ConnectionImpl.java
@@ -4,24 +4,27 @@ import java.io.IOException;
 
 import com.coderising.download.api.Connection;
 
-public class ConnectionImpl implements Connection{
+public class ConnectionImpl implements Connection
+{
 
 	@Override
-	public byte[] read(int startPos, int endPos) throws IOException {
-		
+	public byte[] read(int startPos, int endPos) throws IOException
+	{
+
 		return null;
 	}
 
 	@Override
-	public int getContentLength() {
-		
+	public int getContentLength()
+	{
+
 		return 0;
 	}
 
 	@Override
-	public void close() {
-		
-		
+	public void close()
+	{
+
 	}
 
 }
--- group27/513274874/homework/src/com/coding/coderising/download/impl/ConnectionManagerImpl.java
+++ group27/513274874/homework/src/com/coding/coderising/download/impl/ConnectionManagerImpl.java
@@ -1,14 +1,33 @@
 package com.coderising.download.impl;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import com.coderising.download.api.Connection;
 import com.coderising.download.api.ConnectionException;
 import com.coderising.download.api.ConnectionManager;
 
-public class ConnectionManagerImpl implements ConnectionManager {
+public class ConnectionManagerImpl implements ConnectionManager
+{
 
 	@Override
-	public Connection open(String url) throws ConnectionException {
+	public Connection open(String url) throws ConnectionException
+	{
 		
+		try
+		{
+			URL url1 = new URL(url);
+			url1.openConnection();
+		} catch (MalformedURLException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
 
